4aa4b76d260a39cd479fe7badb53383f72c9fb4a
prayag <prayag@prayag.(none)>
2013-07-11 18:07:54 +0545

    ADD doctrification to Tree

--- application/models/Tree.php
+++ application/models/Tree.php
@@ -1,19 +1,50 @@
 <?php
+namespace models;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+  * @ORM\Entity(repositoryClass="models\repository\TreeRepository")
+  * @ORM\Table(name="Tree")
+  */
 
 class Tree {
-
+    /*
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue                                     
+     */
     private $id;
 
+    /*
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
     private $name;
 
+    /*
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
     private $scientificName;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $localName;
-
+    
+    /*
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $description;
 
+    /*
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
     private $picture;
 
+    /*
+     * @ORM\Column(type="integer")
+     */
     private $lifespan;
 }
 ?>
