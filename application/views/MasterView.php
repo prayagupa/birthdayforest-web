de68769793f4ed7001fb8546f35ccc07ddb224fd
iPrayag <prayag.upd@gmail.com>
2013-07-15 01:08:31 +0545

    REFACTOR views

--- application/views/MasterView.php
+++ application/views/MasterView.php
@@ -2,12 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Plantation</title>
-    <meta http-equiv="imagetoolbar" content="no" />
+<title>Plantation</title>
+<meta http-equiv="imagetoolbar" content="no" />
 
-    <meta name="description" content="Contrators job board" />
-    <meta name="keywords" content="Contrators jobs - no signup, no ads - just new jobs." />
+<meta name="description" content="birthday" />
 
+<link href="<?php print base_url()."assest/css/bootstrap.min.css"?> rel="stylesheet" />
+<link href="<?php print base_url()."assest/css/bootstrap-responsive.min.css"?> rel="stylesheet" />
+
+<script type="text/javascript" src="<?php print base_url()."assest/js/bootstrap.min.js"?>"></script>
 <script src="<?php print base_url()."assest/js/jquery.js"?>"></script>
 </head>
 <body>
@@ -18,7 +21,7 @@
 			<input name="username"/>
 	    </div>
 	     <div id="wrapper" class="pw align-c">
-			<input name="request" type="submit" value="pay"/>
+			<input name="request" type="submit" value="pay" class="btn btn-success"/>
 	     </div>
 	<?php echo form_close();?>
  <script>
